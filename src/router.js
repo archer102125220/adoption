39048c0057118cd57686462dab17fe4d2daa4832
Archer <archer102125220.2015@gmail.com>
2019-11-18 18:55:04 +0800

    updata

--- src/router.js
+++ src/router.js
@@ -1,15 +1,72 @@
-import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
+import React, { Component } from 'react';
+import { Route, Switch, routerRedux, withRouter, Redirect } from 'dva/router';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
 import IndexPage from './routes/IndexPage';
+const { ConnectedRouter } = routerRedux;
 
-function RouterConfig({ history }) {
+const routeComponent = [
+  { key: 'root', path: '/index', exact: true, component: IndexPage },
+];
+const redirectComponent = [ //轉址
+  { key: 'root', exact: true, to: '/index', From: '/' },
+];
+
+class Root extends Component {
+  static propTypes = {
+    app: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired,
+    children: PropTypes.any
+  };
+
+  render() {
+    const { children } = this.props;
+    return children;
+  }
+}
+
+const RouterRoot = withRouter(_.flow()(Root));
+
+const renderRoutes = (r) => {
+  const { key, exact, path, component: Component } = r;
   return (
-    <Router history={history}>
-      <Switch>
-        <Route path="/" exact component={IndexPage} />
-      </Switch>
-    </Router>
+    <Route
+      key={`route-${key}`}
+      exact={exact}
+      path={path}
+      render={(props) => <Component {...props} />}
+    />
+  );
+}
+const renderRedirects = (r) => {
+  const { key, exact, to, From } = r;
+  return (
+    <Redirect
+      key={`redirect-${key}`}
+      exact={exact}
+      from={From}
+      to={to}
+    />
   );
 }
 
-export default RouterConfig;
+const router = props => {
+  return (
+    <ConnectedRouter {...props}>
+      <RouterRoot {...props}>
+        <Switch>
+          {/* <AppSwitch> */}
+          {
+            routeComponent.map(value => renderRoutes(value))
+          }
+          {
+            redirectComponent.map(value => renderRedirects(value))
+          }
+          {/* </AppSwitch> */}
+        </Switch>
+      </RouterRoot>
+    </ConnectedRouter>
+  );
+};
+
+export default router;
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'dva';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import { Route, Switch, routerRedux, withRouter, Redirect } from 'dva/router';
-import styles from './IndexPage.css';
+import _ from 'lodash';
 
-function IndexPage() {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
-      <img src='yay.jpg' alt='' />
-    </div>
-  );
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        11
+      </div>
+    );
+  }
 }
-
-IndexPage.propTypes = {
-};
-
-export default connect()(IndexPage);
